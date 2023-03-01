1f4a1d8e96e585a0dd8f7f9d211d9b9587559dca
Siteech11 <110279646+Siteech11@users.noreply.github.com>
2023-03-01 22:36:07 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import openai
+import os
+from datetime import datetime
 
 # Replace YOUR_TOKEN_HERE with your bot's API token
 bot = telegram.Bot(token='6082427393:AAFjUF7VLejvM13FagNNoWLtqmM08LuoyA0')
+
+aienv = os.getenv('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')
+openai.api_key = "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"
+completion = openai.Completion()
 
 # Define a command handler for /start command
 def start(update, context):
@@ -10,7 +17,21 @@ def start(update, context):
 
 # Define a message handler for text messages
 def echo(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=ask(update.message.text))
+
+def ask(question, chat_log=None):
+    if chat_log is None:
+        chat_log = 'The following is a chat between two users:\n\n'
+    now = datetime.now()
+    ampm = now.strftime("%M:%I %I")
+    t = '[' + ampm + ']'
+    prompt = f'{t}:{question}\n{t}:'
+    response = completion.create(
+        prompt=prompt, engine="text-davinci-003", stop=['\n'], temperature=0.7,
+        top_p=1, frequency_penalty=0, presence_penalty=0, best_of=1,
+        max_tokens=250)
+    answer = response.choices[0].text.strip()
+    return answer
 
 # Create an Updater object and attach the command and message handlers to it
 updater = Updater(token='6082427393:AAFjUF7VLejvM13FagNNoWLtqmM08LuoyA0',
